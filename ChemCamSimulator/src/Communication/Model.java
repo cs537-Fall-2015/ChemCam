2483d665a024d425d5254f9ac4db182641745b40
Jaymin <James.patel37@gmail.com>
2015-10-31 15:02:17 -0700

    Add two new classes that works as a communication between gui and controller

---
+++ ChemCamSimulator/src/Communication/Model.java
@@ -0,0 +1,23 @@
+package Communication;
+
+public class Model {
+
+	String commands;
+
+
+	public Model(String commands) {
+		super();
+		this.commands = commands;
+	}
+
+	public String getCommands() {
+		return commands;
+	}
+
+	public void setCommands(String commands) {
+		this.commands = commands;
+	}
+	
+	
+
+}
