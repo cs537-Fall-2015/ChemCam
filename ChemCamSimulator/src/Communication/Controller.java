ceef7278c582033d82d1a76568d452979b650aed
sridharharsha <malavaiah.sridhar@gmail.com>
2015-11-05 23:10:08 -0800

    Added the commands to the list

--- ChemCamSimulator/src/Communication/Controller.java
+++ ChemCamSimulator/src/Communication/Controller.java
@@ -1,4 +1,4 @@
-package Communication;
+package communication;
 
 import java.util.ArrayList;
 
@@ -7,6 +7,16 @@ public class Controller {
 	public static void main(String[] args) {
 
 		ArrayList<Model> list = new ArrayList<Model>();
+		list.add(new Model("CCAM_POWER_ON"));
+		list.add(new Model("CCAM_COOLER_ON"));
+		list.add(new Model("CCAM_LASER_ON"));
+		list.add(new Model("CCAM_CWL_WARM"));
+		list.add(new Model("CCAM_SET_FOCUS"));
+		list.add(new Model("CCAM_LIBS_WARM"));
+		list.add(new Model("CCAM_FIRE_LASER"));
+		list.add(new Model("CCAM_LASER_OFF"));
+		list.add(new Model("CCAM_COOLER_OFF"));
+		list.add(new Model("CCAM_POWER_OFF"));
 	}
 
 }
